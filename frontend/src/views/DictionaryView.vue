874ed1783a9d260638f2377f858f8555412637d6
César Belley <cesar.belley@lse.epita.fr>
2025-03-22 19:21:36 +0100

    frontend: Add user connection on dico view

--- frontend/src/views/DictionaryView.vue
+++ frontend/src/views/DictionaryView.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { useUserStore } from '@/stores/user'
+import { useVocabStore } from '@/stores/vocabulary'
 import NavigationBar from '@/components/NavigationBar.vue'
 import DictionaryTable from '@/components/DictionaryTable.vue'
+
+// Access the vocab and user store.
+const vocabStore = useVocabStore()
+const userStore = useUserStore()
+
+// Fetch user data when the component mounts.
+onMounted(async () => {
+  if (!userStore.isFetched) {
+    await userStore.fetchUser()
+  }
+  if (userStore.user) {
+    await vocabStore.loadVocabulary()
+  }
+})
 </script>
 
 <template>
