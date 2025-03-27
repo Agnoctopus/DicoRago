533cad9585f16c9fd2afc66e4159e9365dd8cce9
César Belley <cesar.belley@lse.epita.fr>
2025-03-27 20:09:33 +0100

    frontend: Log info instead of error when detail

--- frontend/src/stores/user.ts
+++ frontend/src/stores/user.ts
@@ -20,11 +20,17 @@ export const useUserStore = defineStore('user', () => {
     } catch (error: unknown) {
       let message = 'Unknown error'
       if (axios.isAxiosError(error)) {
-        message = error.response?.data?.detail || error.message
+        const detail = error.response?.data?.detail
+        if (detail) {
+          console.info("Couldn't fetch the user:", detail)
+        } else {
+          message = error.message
+          console.error('Error fetching user:', message)
+        }
       } else if (error instanceof Error) {
         message = error.message
+        console.error('Error fetching user:', message)
       }
-      console.error('Error fetching user:', message)
       user.value = null
     }
     isFetched.value = true
