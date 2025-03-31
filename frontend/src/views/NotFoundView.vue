9cab70ff48f880c292da5b9d1370896c92dcf96e
César Belley <cesar.belley@lse.epita.fr>
2025-03-31 21:46:01 +0200

    frontend: Fix 404 style

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+  <div class="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-4">
     <!-- Icon of the application -->
     <img :src="icon" alt="" class="w-56 h-56 mb-6 rounded-4xl border-2" />
     <h1 class="text-5xl font-bold text-gray-800 mb-4">404</h1>
-    <p class="text-xl text-gray-700 mb-6">Oops! The page you are looking for doesn't exist.</p>
+    <p class="text-xl text-gray-700 mb-6 text-center">
+      Oops! The page you are looking for doesn't exist.
+    </p>
     <router-link to="/" class="text-blue-600 hover:underline"> Return to Home </router-link>
   </div>
 </template>
@@ -12,5 +14,4 @@
 import icon from '@/assets/icon.png'
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
