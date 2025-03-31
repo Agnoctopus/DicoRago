bb42853577f578595dfb8e7c6cc89da0dcac035b
César Belley <cesar.belley@lse.epita.fr>
2025-03-31 21:31:59 +0200

    frontend: Add app Icon in 404 page

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+    <!-- Icon of the application -->
+    <img :src="icon" alt="" class="w-56 h-56 mb-6 rounded-4xl border-2" />
     <h1 class="text-5xl font-bold text-gray-800 mb-4">404</h1>
-    <p class="text-xl text-gray-600 mb-6">Oops! The page you are looking for doesn't exist.</p>
-    <router-link to="/" class="text-blue-500 hover:underline"> Return to Home </router-link>
+    <p class="text-xl text-gray-700 mb-6">Oops! The page you are looking for doesn't exist.</p>
+    <router-link to="/" class="text-blue-600 hover:underline"> Return to Home </router-link>
   </div>
 </template>
 
 <script setup lang="ts">
-// No additional logic needed
+import icon from '@/assets/icon.png'
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
